8da7724c51e385dd2ce2c8b3106ef311cf742751
ryoyakawai <ryoya.kawai@gmail.com>
2018-06-17 21:12:45 -0700

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* sw.js */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-let VERSION = '1.0.17';
+let VERSION = '1.0.18';
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded `);
