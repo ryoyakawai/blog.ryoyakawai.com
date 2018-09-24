146f88fd9ab66b65bec29c5c078e4ad0d06e427c
Ryoya Kawai <ryoya.kawai@gmail.com>
2018-09-24 00:02:09 -0700

    add: entry

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* sw.js */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-let VERSION = '1.0.24';
+let VERSION = '1.0.25';
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded `);
