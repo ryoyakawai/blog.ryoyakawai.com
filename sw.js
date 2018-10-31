f05927ac7c929164155b07dc4e4ce7a14ee299f5
Ryoya Kawai <ryoya.kawai@gmail.com>
2018-10-31 22:10:18 +0900

    update: version number of sw

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* sw.js */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-let VERSION = '2.0.0';
+let VERSION = '2.0.1';
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded `);
